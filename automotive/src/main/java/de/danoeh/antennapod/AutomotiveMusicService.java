b41565d01c4070b3eca6ba433bc76e11513efdb8
Lukas Hoffmann <lhoff@zedat.fu-berlin.de>
2019-11-14 01:32:48 +0100

    Creating seperate Automotive Module Add Settings Activity Populate settings with dummy data

---
+++ automotive/src/main/java/de/danoeh/antennapod/AutomotiveMusicService.java
@@ -0,0 +1,15 @@
+package de.danoeh.antennapod;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+
+public class AutomotiveMusicService extends Service {
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
---
+++ automotive/src/main/java/de/danoeh/antennapod/SettingsFragment.kt
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2019 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.danoeh.antennapod
+
+import android.app.Application
+import android.content.ComponentName
+import android.os.Bundle
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModelProviders
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
+
+
+/**
+ * Preference fragment hosted by [SettingsActivity]. Handles events to various preference changes.
+ */
+class SettingsFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preferences, rootKey)
+    }
+
+
+}
+
+/**
+ * Basic ViewModel for [SettingsFragment].
+
+
+class SettingsFragmentViewModel(application: Application) : AndroidViewModel(application) {
+    private val applicationContext = application.applicationContext
+    private val mediaSessionConnection = MediaSessionConnection(
+        applicationContext,
+        ComponentName(applicationContext, AutomotiveMusicService::class.java)
+    )
+
+    fun logout() {
+        // Logout is fire and forget.
+        mediaSessionConnection.sendCommand(LOGOUT, null)
+    }
+}
+ */
